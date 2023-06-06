ac2af02901b3f8a50d497e0646ba84d257bf9777
Andi Rommy Rizky Risyandi <42740491+RommyRisyandi@users.noreply.github.com>
2023-06-06 08:37:21 +0700

    first commit

---
+++ README.txt
@@ -0,0 +1,21 @@
+NEWPROJECT - Site Template
+
+**NEW PROJECT - is a fully responsive site template.** it was developed with HTML,CSS,JS and Google Material Design (https://material.google.com) of powers.
+
+
+Demo
+------------
+You can check the website preview with ***[this link](http://newproject.000.pe/)**
+
+Browser Support
+------------
+- IE 10 +
+- Microsoft Edge (Latest)
+- Mozilla Firefox (Latest)
+- Google Chrome (Latest)
+- Opera (Latest)
+- Safari (Latest)
+
+License
+------------
+**NEWPROJECT - Site Template** is an paid source project that is created by AR3
---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// Mobile Navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(
+  ".section-hero, .about-us, .section-cta"
+);
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// fix in Safari
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// loading section
+window.addEventListener("load", () => {
+  document.querySelector("lds-spinner").classList.add("lds-spinner--hidden");
+});
